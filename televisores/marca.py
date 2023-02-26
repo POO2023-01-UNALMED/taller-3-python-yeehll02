5c097582af84d0e2e977ee384e3be7ea6857d1e4
yeehll02 <116982357+yeehll02@users.noreply.github.com>
2023-02-25 19:44:02 -0500

    taller3

---
+++ televisores/marca.py
@@ -0,0 +1,12 @@
+class Marca:
+    
+    def __init__(self, nombre):
+        self._nombre = nombre
+
+
+    def getNombre(self):
+        return self._nombre
+    def set(self,nombre):
+        self._nombre=nombre
+        
+        
